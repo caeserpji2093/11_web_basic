6ac70e93c5f6f4f9f81c80a415e65a7087e325c4
Caeser Petronius Jupiter <caeserpji2093@gmail.com>
2022-09-28 14:45:10 +0900

    update 20220928 1445 hours

---
+++ 11_web_basic/WebContent/03_Javascript/chapter01_basic/example.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+
+document.write("<h1>외부 Javascript</h1>");
+document.write("<hr>");
+
+console.log("외부 자바스크립트1")
+console.log("외부 자바스크립트2")
+console.log("외부 자바스크립트3")
+
+alert("반갑습니다.")
+alert("외부 Javascript 입니다.")
